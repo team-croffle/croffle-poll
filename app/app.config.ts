8f39aa18497d55e662ee5e8a0d92378655379e66
BlueNyang <xpsj20@gmail.com>
2026-04-06 00:12:20 +0900

    chore: nuxt project
    Signed-off-by: BlueNyang <xpsj20@gmail.com>

---
+++ app/app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  title: 'Croffle Dev. Vote',
+  theme: {
+    dark: true,
+  },
+  ui: {
+    colors: {
+      primary: 'cyan',
+      secondary: 'slate',
+      neutral: 'gray',
+      tertiary: 'emerald',
+    },
+  },
+});
---
+++ eslint.config.mjs
@@ -0,0 +1,63 @@
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+import pluginPromise from 'eslint-plugin-promise';
+import pluginUnicorn from 'eslint-plugin-unicorn';
+
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default withNuxt(
+  {
+    plugins: {
+      promise: pluginPromise,
+      unicorn: pluginUnicorn,
+    },
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            pascalCase: true,
+            camelCase: true,
+          },
+          ignore: [
+            /.*~\d{8}-\d{6}\..*$/, // Syncthing
+          ],
+        },
+      ],
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/prefer-top-level-await': 'off',
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/attributes-order': [
+        'warn',
+        {
+          alphabetical: false,
+          order: [
+            'DEFINITION',
+            'LIST_RENDERING',
+            'CONDITIONALS',
+            'RENDER_MODIFIERS',
+            'GLOBAL',
+            ['UNIQUE', 'SLOT'],
+            'TWO_WAY_BINDING',
+            'OTHER_DIRECTIVES',
+            'OTHER_ATTR',
+            'EVENTS',
+            'CONTENT',
+          ],
+        },
+      ],
+      'vue/component-api-style': ['error', ['script-setup', 'composition']],
+      'vue/define-props-declaration': ['error', 'type-based'],
+      'vue/multi-word-component-names': 'off',
+      'vue/no-v-html': 'warn',
+    },
+  },
+  eslintConfigPrettier
+);
